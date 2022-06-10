c87b4300eb13c62c41250bc6eb5062a6af13ee8f
Amanda Hefner <bswj90@gmail.com>
2022-06-09 23:46:10 -0400

    added flordia filter and started working out timer algorithm

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -7,7 +7,7 @@ let getAPIdata = (cityName) => {
 
   fetch(apiUrl).then(function (response) {
     response.json().then(function (data) {
-      console.log(data);
+      //console.log(data);
     });
   });
 };
@@ -16,81 +16,32 @@ getAPIdata("Miami");
 getAPIdata("Orlando");
 
 let launch = () => {
-  let rocketApi = "https://lldev.thespacedevs.com/2.2.0/launch/upcoming";
-  let rocketDisplay = document.querySelector(".rocket-display");
+  let rocketApi =
+    "https://lldev.thespacedevs.com/2.2.0/launch/upcoming?limit=25";
 
   fetch(rocketApi).then((response) => {
     if (response.ok) {
       response.json().then((data) => {
         var dataArray = data.results;
+        var filteredArray = [];
 
-        for (i = 0; i < dataArray.length; i++) {
-          var rocket = data.results[i].rocket.configuration.full_name;
-          var rocketImgUrl = data.results[i].image;
-          var launchLocation = data.results[i].pad.location.name;
-          var launchTime = data.results[i].net;
-          var launchStatus = data.results[i].status.name;
-          var statusAbbrev = data.results[i].status.abbrev;
-
-          var card = document.createElement("div");
-          card.setAttribute("class", "rocket-card");
-
-          var rocketNameEl = document.createElement("h4");
-          rocketNameEl.setAttribute("class", "rocket-name");
-          rocketNameEl.innerText = rocket;
-          card.appendChild(rocketNameEl);
-
-          var rocketImgEl = document.createElement("img");
-          rocketImgEl.setAttribute("src", rocketImgUrl);
-          card.appendChild(rocketImgEl);
-
-          var launchLocationEl = document.createElement("h3");
-          launchLocationEl.innerText = launchLocation;
-          card.appendChild(launchLocationEl);
-
-          var statusContainerEl = document.createElement("div");
-          statusContainerEl.setAttribute("class", "wrapper");
-          card.appendChild(statusContainerEl);
-
-          var statusSpanEl = document.createElement("span");
-          statusSpanEl.innerText = "Status: ";
-          statusContainerEl.appendChild(statusSpanEl);
-
-          var launchStatusEl = document.createElement("p");
-
-          switch (statusAbbrev) {
-            case "Go":
-              launchStatusEl.setAttribute("class", "good-color");
-              break;
-            case "TBD":
-              launchStatusEl.setAttribute("class", "bad-color");
-              break;
-            case "TBC":
-              launchStatusEl.setAttribute("class", "no-color");
-              break;
-            case "In Flight":
-              launchLocationEl.setAttribute("class", "flight-color");
-              break;
+        for (j = 0; j < dataArray.length; j++) {
+          if (
+            dataArray[j].pad.location.id === 27 ||
+            dataArray[j].pad.location.id === 12
+          ) {
+            filteredArray.push(dataArray[j]);
           }
-
-          launchStatusEl.innerText = launchStatus;
-          statusContainerEl.appendChild(launchStatusEl);
-
-          var launchTimeEl = document.createElement("p");
-
-          var convertedTime = countDown(launchTime, statusAbbrev);
-
-          launchTimeEl.innerText = convertedTime;
-          card.appendChild(launchTimeEl);
-
-          rocketDisplay.appendChild(card);
         }
+        launchAppend(filteredArray);
       });
     }
   });
 };
 
-var countDown = (launchTime, statusAbbrev) => {
+var countDown = (launchTime, statusAbbrev, i) => {
+  var launchTimeEle = document.getElementsByClassName("launch-time");
+  //console.log(launchTimeEle);
   let getTime = launchTime.slice(11);
   getTime = getTime.split("Z");
   getTime = getTime[0].split(":");
@@ -112,18 +63,87 @@ var countDown = (launchTime, statusAbbrev) => {
   var launchMonth = parseInt(launchDate[1]);
   var launchDay = parseInt(launchDate[2]);
 
+  //console.log("launch", launchYear, launchMonth, launchDay, statusAbbrev);
+  //console.log("time", theYear, theMonth, theDate);
+
   if (
     statusAbbrev === "Go" &&
     launchYear === theYear &&
     launchMonth === theMonth &&
     launchDay === theDate
   ) {
-    console.log("something met this criteria!");
+    if (hours < theHour) {
+      launchTimeEle[i].innerText = "[ T- 00 : 00 : 00 ]";
+      console.log("the else ran!");
+    }
+  } else {
+    launchTimeEle[i].innerText =
+      "[ T- " + hours + " : " + minutes + " : " + seconds + " ]";
   }
+};
 
-  var timeParsed = "[ T- " + hours + " : " + minutes + " : " + seconds + " ]";
+var launchAppend = function (dataArray) {
+  let rocketDisplay = document.querySelector(".rocket-display");
+  for (i = 0; i < dataArray.length; i++) {
+    var rocket = dataArray[i].rocket.configuration.full_name;
+    var rocketImgUrl = dataArray[i].image;
+    var launchLocation = dataArray[i].pad.location.name;
+    var launchTime = dataArray[i].net;
+    var launchStatus = dataArray[i].status.name;
+    var statusAbbrev = dataArray[i].status.abbrev;
 
-  return timeParsed;
+    var card = document.createElement("div");
+    card.setAttribute("class", "rocket-card");
+
+    var rocketNameEl = document.createElement("h4");
+    rocketNameEl.setAttribute("class", "rocket-name");
+    rocketNameEl.innerText = rocket;
+    card.appendChild(rocketNameEl);
+
+    var rocketImgEl = document.createElement("img");
+    rocketImgEl.setAttribute("src", rocketImgUrl);
+    card.appendChild(rocketImgEl);
+
+    var launchLocationEl = document.createElement("h3");
+    launchLocationEl.innerText = launchLocation;
+    card.appendChild(launchLocationEl);
+
+    var statusContainerEl = document.createElement("div");
+    statusContainerEl.setAttribute("class", "wrapper");
+    card.appendChild(statusContainerEl);
+
+    var statusSpanEl = document.createElement("span");
+    statusSpanEl.innerText = "Status: ";
+    statusContainerEl.appendChild(statusSpanEl);
+
+    var launchStatusEl = document.createElement("p");
+
+    switch (statusAbbrev) {
+      case "Go":
+        launchStatusEl.setAttribute("class", "good-color");
+        break;
+      case "TBD":
+        launchStatusEl.setAttribute("class", "bad-color");
+        break;
+      case "TBC":
+        launchStatusEl.setAttribute("class", "no-color");
+        break;
+      case "In Flight":
+        launchLocationEl.setAttribute("class", "flight-color");
+        break;
+    }
+
+    launchStatusEl.innerText = launchStatus;
+    statusContainerEl.appendChild(launchStatusEl);
+
+    var launchTimeEl = document.createElement("p");
+    launchTimeEl.setAttribute("class", "launch-time");
+    card.appendChild(launchTimeEl);
+
+    rocketDisplay.appendChild(card);
+
+    countDown(launchTime, statusAbbrev, i);
+  }
 };
 
 launch();
