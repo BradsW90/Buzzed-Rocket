68a92237b46109a6bc5791132e0bdbf47e071c20
Amanda Hefner <bswj90@gmail.com>
2022-06-11 22:05:42 -0400

    finished card display paused working on timer

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,28 +1,11 @@
-// API - generic
-let getAPIdata = (cityName) => {
-  var apiUrl =
-    "https://api.openbrewerydb.org/breweries?by_city=" +
-    cityName +
-    "&per_page=10";
-
-  fetch(apiUrl).then(function (response) {
-    response.json().then(function (data) {
-      //console.log(data);
-    });
-  });
-};
-
-getAPIdata("Miami");
-getAPIdata("Orlando");
-
 let launch = () => {
-  let rocketApi =
-    "https://lldev.thespacedevs.com/2.2.0/launch/upcoming?limit=25";
+  let rocketApi = "https://ll.thespacedevs.com/2.2.0/launch/upcoming?limit=25";
 
   fetch(rocketApi).then((response) => {
     if (response.ok) {
       response.json().then((data) => {
         var dataArray = data.results;
+        //console.log(dataArray);
         var filteredArray = [];
 
         for (j = 0; j < dataArray.length; j++) {
@@ -39,9 +22,11 @@ let launch = () => {
   });
 };
 
-var countDown = (launchTime, statusAbbrev, i) => {
+/*var countDown = (launchTime, statusAbbrev, i) => {
   var launchTimeEle = document.getElementsByClassName("launch-time");
-  //console.log(launchTimeEle);
+
+  //console.log(launchTime);
+
   let getTime = launchTime.slice(11);
   getTime = getTime.split("Z");
   getTime = getTime[0].split(":");
@@ -49,38 +34,65 @@ var countDown = (launchTime, statusAbbrev, i) => {
   var minutes = getTime[1];
   var seconds = getTime[2];
 
-  let fullDate = new Date();
-  var theDate = fullDate.getDate();
-  var theMonth = fullDate.getMonth() + 1;
-  var theYear = fullDate.getFullYear();
-  var theHour = fullDate.getHours() + 1;
-  var theMinute = fullDate.getMinutes() + 1;
-  var theSecond = fullDate.getSeconds() + 1;
-
   let launchDate = launchTime.split("T");
   launchDate = launchDate[0].split("-");
-  var launchYear = parseInt(launchDate[0]);
-  var launchMonth = parseInt(launchDate[1]);
-  var launchDay = parseInt(launchDate[2]);
+  var launchYear = launchDate[0];
+  var launchMonth = launchDate[1];
+  var launchDay = launchDate[2];
 
-  //console.log("launch", launchYear, launchMonth, launchDay, statusAbbrev);
-  //console.log("time", theYear, theMonth, theDate);
+  var launchTimeString = new Date(
+    launchMonth +
+      " " +
+      launchDay +
+      ", " +
+      launchYear +
+      " " +
+      hours +
+      ":" +
+      minutes +
+      ":" +
+      seconds
+  ).getTime();
+
+  console.log(
+    "launch",
+    launchYear,
+    launchMonth,
+    launchDay,
+    statusAbbrev,
+    seconds
+  );
+  //console.log("time", launchTimeString);
 
   if (
     statusAbbrev === "Go" &&
-    launchYear === theYear &&
-    launchMonth === theMonth &&
-    launchDay === theDate
+    launchYear == theYear &&
+    launchMonth == theMonth &&
+    launchDay == theDate
   ) {
-    if (hours < theHour) {
-      launchTimeEle[i].innerText = "[ T- 00 : 00 : 00 ]";
-      console.log("the else ran!");
-    }
+    var intervalId = setInterval(function () {
+      var now = new Date().getTime();
+      var timeToLaunch = launchTimeString - now;
+
+      var hoursLeft = Math.floor(
+        (now % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutesLeft = Math.floor((now % (1000 * 60 * 60)) / (1000 * 60));
+      var secondsLeft = Math.floor((now % (1000 * 60)) / 1000);
+      console.log(
+        "Hours left: " +
+          hoursLeft +
+          " Minutes left: " +
+          minutesLeft +
+          " Seconds left: " +
+          secondsLeft
+      );
+    }, 1000);
   } else {
     launchTimeEle[i].innerText =
       "[ T- " + hours + " : " + minutes + " : " + seconds + " ]";
   }
-};
+};*/
 
 var launchAppend = function (dataArray) {
   let rocketDisplay = document.querySelector(".rocket-display");
@@ -91,6 +103,19 @@ var launchAppend = function (dataArray) {
     var launchTime = dataArray[i].net;
     var launchStatus = dataArray[i].status.name;
     var statusAbbrev = dataArray[i].status.abbrev;
+
+    let getTime = launchTime.slice(11);
+    getTime = getTime.split("Z");
+    getTime = getTime[0].split(":");
+    var hours = getTime[0];
+    var minutes = getTime[1];
+    var seconds = getTime[2];
+
+    let launchDate = launchTime.split("T");
+    launchDate = launchDate[0].split("-");
+    var launchYear = launchDate[0];
+    var launchMonth = launchDate[1];
+    var launchDay = launchDate[2];
 
     var card = document.createElement("div");
     card.setAttribute("class", "rocket-card");
@@ -136,13 +161,20 @@ var launchAppend = function (dataArray) {
     launchStatusEl.innerText = launchStatus;
     statusContainerEl.appendChild(launchStatusEl);
 
+    var launchDateEl = document.createElement("p");
+    launchDateEl.innerText =
+      "Launch Date: " + launchMonth + " / " + launchDay + " / " + launchYear;
+    card.appendChild(launchDateEl);
+
     var launchTimeEl = document.createElement("p");
     launchTimeEl.setAttribute("class", "launch-time");
+    launchTimeEl.innerText =
+      "[ T- " + hours + " : " + minutes + " : " + seconds + " ]";
     card.appendChild(launchTimeEl);
 
     rocketDisplay.appendChild(card);
 
-    countDown(launchTime, statusAbbrev, i);
+    //countDown(launchTime, statusAbbrev, i);
   }
 };
 
