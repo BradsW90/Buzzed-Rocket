8756ebe79d465d25b4338465255915344300f89d
Amanda Hefner <bswj90@gmail.com>
2022-06-15 20:10:53 -0400

    got the mobile display of rocket cards working on screen load

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,106 +1,119 @@
+//gets current screen width
 var screensWidth = window.innerWidth;
 
 //gets html element to create the cards in
 let rocketDisplay = document.querySelector(".rocket-display");
 
-var launchAppend = function (dataArray, arrayLength) {
-  for (i = 0; i < arrayLength; i++) {
-    //variable library for data on cards
-    var rocket = dataArray[i].rocket.configuration.full_name;
-    var rocketImgUrl = dataArray[i].image;
-    var launchLocation = dataArray[i].pad.location.name;
-    var launchTime = dataArray[i].net;
-    var launchStatus = dataArray[i].status.name;
-    var statusAbbrev = dataArray[i].status.abbrev;
+//handles single card generation and correct new element layout
+var miniCard = (dataArray, index) => {
+  //create new card
+  launchAppend(dataArray, index);
 
-    let getTime = launchTime.slice(11);
-    getTime = getTime.split("Z");
-    getTime = getTime[0].split(":");
-    var hours = getTime[0];
-    var minutes = getTime[1];
-    var seconds = getTime[2];
+  //create next button
+  var nextBtn = document.createElement("button");
+  nextBtn.setAttribute("id", "next-btn");
+  nextBtn.innerText = ">";
+  rocketDisplay.appendChild(nextBtn);
+};
 
-    let launchDate = launchTime.split("T");
-    launchDate = launchDate[0].split("-");
-    var launchYear = launchDate[0];
-    var launchMonth = launchDate[1];
-    var launchDay = launchDate[2];
+var launchAppend = function (dataArray, index) {
+  //variable library for data on cards
+  var rocket = dataArray.rocket.configuration.full_name;
+  var rocketImgUrl = dataArray.image;
+  var launchLocation = dataArray.pad.location.name;
+  var launchTime = dataArray.net;
+  var launchStatus = dataArray.status.name;
+  var statusAbbrev = dataArray.status.abbrev;
 
-    //starts card creation
-    var card = document.createElement("div");
-    card.setAttribute("class", "rocket-card");
+  //getting the hours, minutes and seconds of the launch time
+  let getTime = launchTime.slice(11);
+  getTime = getTime.split("Z");
+  getTime = getTime[0].split(":");
+  var hours = getTime[0];
+  var minutes = getTime[1];
+  var seconds = getTime[2];
 
-    //card title
-    var rocketNameEl = document.createElement("h4");
-    rocketNameEl.setAttribute("class", "rocket-name");
-    rocketNameEl.innerText = rocket;
-    card.appendChild(rocketNameEl);
+  //getting the launch year, month, and day
+  let launchDate = launchTime.split("T");
+  launchDate = launchDate[0].split("-");
+  var launchYear = launchDate[0];
+  var launchMonth = launchDate[1];
+  var launchDay = launchDate[2];
 
-    //card img
-    var rocketImgEl = document.createElement("img");
-    rocketImgEl.setAttribute("src", rocketImgUrl);
-    card.appendChild(rocketImgEl);
+  //starts card creation
+  var card = document.createElement("div");
+  card.setAttribute("class", "rocket-card");
+  card.dataset.index = index;
 
-    //launch location
-    var launchLocationEl = document.createElement("h3");
-    launchLocationEl.innerText = launchLocation;
-    card.appendChild(launchLocationEl);
+  //card title
+  var rocketNameEl = document.createElement("h4");
+  rocketNameEl.setAttribute("class", "rocket-name");
+  rocketNameEl.innerText = rocket;
+  card.appendChild(rocketNameEl);
 
-    //div class for launch status
-    var statusContainerEl = document.createElement("div");
-    statusContainerEl.setAttribute("class", "wrapper");
-    card.appendChild(statusContainerEl);
+  //card img
+  var rocketImgEl = document.createElement("img");
+  rocketImgEl.setAttribute("src", rocketImgUrl);
+  card.appendChild(rocketImgEl);
 
-    //span for first part of status
-    var statusSpanEl = document.createElement("span");
-    statusSpanEl.innerText = "Status: ";
-    statusContainerEl.appendChild(statusSpanEl);
+  //launch location
+  var launchLocationEl = document.createElement("h3");
+  launchLocationEl.innerText = launchLocation;
+  card.appendChild(launchLocationEl);
 
-    //p element for acutal launch status
-    var launchStatusEl = document.createElement("p");
+  //div class for launch status
+  var statusContainerEl = document.createElement("div");
+  statusContainerEl.setAttribute("class", "wrapper");
+  card.appendChild(statusContainerEl);
 
-    //used to determine color of launch status text
-    switch (statusAbbrev) {
-      case "Go":
-        launchStatusEl.setAttribute("class", "good-color");
-        break;
-      case "TBD":
-        launchStatusEl.setAttribute("class", "bad-color");
-        break;
-      case "TBC":
-        launchStatusEl.setAttribute("class", "no-color");
-        break;
-      case "In Flight":
-        launchLocationEl.setAttribute("class", "flight-color");
-        break;
-    }
+  //span for first part of status
+  var statusSpanEl = document.createElement("span");
+  statusSpanEl.innerText = "Status: ";
+  statusContainerEl.appendChild(statusSpanEl);
 
-    launchStatusEl.innerText = launchStatus;
-    statusContainerEl.appendChild(launchStatusEl);
+  //p element for acutal launch status
+  var launchStatusEl = document.createElement("p");
 
-    //launch date
-    var launchDateEl = document.createElement("p");
-    launchDateEl.innerText =
-      "Launch Date: " + launchMonth + " / " + launchDay + " / " + launchYear;
-    card.appendChild(launchDateEl);
-
-    //launch time
-    var launchTimeEl = document.createElement("p");
-    launchTimeEl.setAttribute("class", "launch-time");
-    launchTimeEl.innerText =
-      "[ T- " + hours + " : " + minutes + " : " + seconds + " ]";
-    card.appendChild(launchTimeEl);
-
-    rocketDisplay.appendChild(card);
-
-    //future call for launch count down
-    //countDown(launchTime, statusAbbrev, i);
+  //used to determine color of launch status text
+  switch (statusAbbrev) {
+    case "Go":
+      launchStatusEl.setAttribute("class", "good-color");
+      break;
+    case "TBD":
+      launchStatusEl.setAttribute("class", "bad-color");
+      break;
+    case "TBC":
+      launchStatusEl.setAttribute("class", "no-color");
+      break;
+    case "In Flight":
+      launchLocationEl.setAttribute("class", "flight-color");
+      break;
   }
+
+  launchStatusEl.innerText = launchStatus;
+  statusContainerEl.appendChild(launchStatusEl);
+
+  //launch date
+  var launchDateEl = document.createElement("p");
+  launchDateEl.innerText =
+    "Launch Date: " + launchMonth + " / " + launchDay + " / " + launchYear;
+  card.appendChild(launchDateEl);
+
+  //launch time
+  var launchTimeEl = document.createElement("p");
+  launchTimeEl.setAttribute("class", "launch-time");
+  launchTimeEl.innerText =
+    "[ T- " + hours + " : " + minutes + " : " + seconds + " ]";
+  card.appendChild(launchTimeEl);
+
+  rocketDisplay.appendChild(card);
+
+  //future call for launch count down
+  //countDown(launchTime, statusAbbrev, i);
 };
 let launch = () => {
   //if needing to test page for anything add dev after the ll
-  let rocketApi =
-    "https://lldev.thespacedevs.com/2.2.0/launch/upcoming?limit=8";
+  let rocketApi = "https://ll.thespacedevs.com/2.2.0/launch/upcoming?limit=8";
 
   //call api
   fetch(rocketApi).then((response) => {
@@ -109,24 +122,92 @@ let launch = () => {
       //turns response into json and then sets up data array for rocket cards
       response.json().then((data) => {
         var dataArray = data.results;
-        var arrayLength = dataArray.length;
+        var index = 0;
         //console.log(dataArray);
 
+        //checking for mobile screen size
         if (screensWidth > 480) {
-          //calls the function to dynamiclly create cards using the new filtered array
-          launchAppend(dataArray, arrayLength);
+          //generates all cards to screen since screen is bigger then mobile
+          for (i = 0; i < dataArray.length; i++) {
+            launchAppend(dataArray[i]);
+          }
+          //starts single creation cards
         } else {
+          //makes previous button element
           var prevBtn = document.createElement("button");
           prevBtn.setAttribute("id", "prev-btn");
           prevBtn.innerText = "<";
           rocketDisplay.appendChild(prevBtn);
 
-          launchAppend(dataArray, 1);
+          //calls mini card to create first card and next button element
+          miniCard(dataArray[index], index);
 
-          var nextBtn = document.createElement("button");
-          nextBtn.setAttribute("id", "next-btn");
-          nextBtn.innerText = ">";
-          rocketDisplay.appendChild(nextBtn);
+          //setup event listener on card container to listen for button presses
+          rocketDisplay.addEventListener("click", (e) => {
+            //if previous button pressed
+            if (e.target.innerText === "<") {
+              var prevParentEl = e.target.parentNode;
+              var prevCurrentEl = prevParentEl.childNodes;
+
+              //checks to see if current card is beginning of array
+              if (parseInt(prevCurrentEl[1].dataset.index) === 0) {
+                //if beginning of array create card from last array data
+                for (c = 0; c < 2; c++) {
+                  prevCurrentEl[1].remove();
+                }
+                miniCard(dataArray[7], 7);
+                return;
+
+                //if current card is not beginning create card based on last data down
+              } else {
+                //gets last index used
+                var prevRocket = prevCurrentEl[1].dataset.index;
+
+                //removes card and nex button
+                for (b = 0; b < 2; b++) {
+                  prevCurrentEl[1].remove();
+                }
+
+                //decriment current index
+                prevRocket--;
+
+                //create new card and next button based on new index
+                miniCard(dataArray[prevRocket], prevRocket);
+              }
+
+              //in next button is pressed
+            } else if (e.target.innerText === ">") {
+              var parentEl = e.target.parentNode;
+              var currentEl = parentEl.childNodes;
+
+              //check if card is the last in array
+              if (parseInt(currentEl[1].dataset.index) === 7) {
+                //remove card and next button
+                for (c = 0; c < 2; c++) {
+                  currentEl[1].remove();
+                }
+
+                //create first card in array
+                miniCard(dataArray[0], 0);
+                return;
+
+                //if card is not first card in array
+              } else {
+                var nextRocket = currentEl[1].dataset.index;
+
+                //remove current element and next button
+                for (a = 0; a < 2; a++) {
+                  currentEl[1].remove();
+                }
+
+                //increment index
+                nextRocket++;
+
+                //call function to create next card in array and recreate next button
+                miniCard(dataArray[nextRocket], nextRocket);
+              }
+            }
+          });
         }
       });
     }
